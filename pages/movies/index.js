ca93c260eba09c67445d65d39c16368e18efa4ee
chris campbell <hopprdev@gmail.com>
2022-08-28 07:54:41 -0400

    clean up

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -2,17 +2,18 @@ import React from "react";
 import DashboardComponent from "../../components/dashboard/DashboardComponent";
 import { useInfiniteQuery } from "react-query";
 import { getSession } from "next-auth/react";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const apiUrl = process.env.API_URL;
   const apiKey = process.env.API_KEY;
+  const popularURL = `${apiUrl}movie/popular?api_key=${apiKey}&language=en-US&page=`;
 
   const { data, status, fetchNextPage, hasNextPage } = useInfiniteQuery(
     "infiniteCharacters",
     async ({ pageParam = 1 }) =>
-      await fetch(
-        `${apiUrl}movie/popular?api_key=${apiKey}&language=en-US&page=${pageParam}`
-      ).then((result) => result.json()),
+      await axios.get(`${popularURL}${pageParam}`).then(({ data }) => data),
     {
       getNextPageParam: (lastPage, pages) => {
         if (lastPage.page < lastPage.total_pages) {
