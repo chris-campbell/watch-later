23a670c417025561d580786108ed42937f26e74b
chris campbell <hopprdev@gmail.com>
2022-08-28 12:28:32 -0400

    Remove hooks folder

--- hooks/useSeachTerm.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const useSearchTerm = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  return { searchTerm, setSearchTerm };
-};
-
-export default useSearchTerm;
